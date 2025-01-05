4bab4533b041affb5d3a0593b29b01bbd631116c
AhmadYousif89 <ayob.4wrk89@gmail.com>
2025-01-05 23:31:22 +0200

    Fix in getBudgetObject function

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
-  return {
+  const budget = {
     income,
     gdp,
     capita,
   };
+
+  return budget;
 }
+console.log(getBudgetObject(400, 700, 900));
