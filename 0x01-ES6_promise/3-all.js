18456af927ee411cb5a1a8a1b1d9d18551f44042
AhmadYousif89 <ayob.4wrk89@gmail.com>
2025-01-08 14:35:01 +0200

    Fix console.log in 2-then.js and handle error in 3-all.js and 6-final-user.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,5 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   const data = Promise.all([uploadPhoto(), createUser()]);
-  data.then(([{ body }, { firstName, lastName }]) => console.log(body, firstName, lastName));
+  return data
+    .then(([{ body }, { firstName, lastName }]) => console.log(body, firstName, lastName))
+    .catch(() => console.log('Signup system offline'));
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((data) =>
+    data.map((entry) => ({
+      status: entry.status,
+      value: entry.status === 'fulfilled' ? entry.value : entry.reason.toString(),
+    })),
+  );
 }
